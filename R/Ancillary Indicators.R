9eb2d59938c64909194ed770c5644da38e4b89de
tcarruth <tomsinbox@gmail.com>
2018-01-18 16:32:16 -0800

    Ancillary indicators pt 3 - first complete power analysis

--- R/Ancillary Indicators.R
+++ R/Ancillary Indicators.R
@@ -14,10 +14,10 @@ AAV<-function(x,mat,ind){
 }
 
 mu<-function(x,mat,ind){
-  mean(mat[x,ind])
+  log(mean(mat[x,ind]))
 }
 
-getinds<-function(PPD,styr,res){
+getinds<-function(PPD,styr,res, tsd= c("Cat","Cat","Cat","Ind","ML"),stat=c("slp","AAV","mu","slp", "slp")){
 
   nsim<-dim(PPD@Cat)[1]
   proyears<-dim(PPD@Cat)[2]-styr+1
@@ -25,8 +25,6 @@ getinds<-function(PPD,styr,res){
   if(res>proyears)message(paste0("The temporal resolution for posterior predictive data calculation (",res,") is higher than the number of projected years (",proyears,"). Only one time step of indicators are calculated for ",proyears, " projected years."))
   np<-floor(proyears/res)
 
-  tsd<- c("Cat","Cat","Cat", "Ind","ML")     #, "Lc")
-  stat<-c("slp","AAV","mu",  "slp",  "slp")  #,"slp")
   ntsd<-length(tsd)
   inds<-array(NA,c(ntsd,np,nsim))
 
@@ -81,36 +79,59 @@ CC<-function(indPPD,indData,pp=1,nams=c("YS", "AAVY","YM","IS","MLS"),res){
 }
 
 Probs<-function(indPPD,indData,alpha=0.05){
-
+ 
+  ntsd<-dim(indPPD)[1]
   np<-dim(indPPD)[2]
   nsim<-dim(indPPD)[3]
-  #PRB<-array(NA,c(4,np)) # True Negative, False Negative, False Positive, True Positive
-  PRB<-array(NA,c(2,np)) # True Negative, False Negative, False Positive, True Positive
+  #PRB<-array(NA,c(4,np)) # False Positive, True Positive
+  PRB<-array(NA,c(2,np))  # False Positive, True Positive
   mah<-array(NA,c(2,np,nsim))
 
   for(pp in 1:np){
+    
+    keep<-array(TRUE,c(ntsd,pp))
+    for(i in 1:ntsd){
+     for(j in 1:pp){ 
+       if(dip(indPPD[i,j,])>0.065)keep[i,j]=FALSE
+      }
+    }
+    
+    ni<-sum(keep)
+    keepind<-as.matrix(expand.grid(1:ntsd,1:pp,1:nsim))[rep(as.vector(keep),nsim),]
 
-    ntsd<-dim(indPPD)[1]
-    ni<-pp*ntsd
-
-    ind3PPD<-t(matrix(indPPD[,1:pp,],nrow=ni))
-    ind3Data<-t(matrix(indData[,1:pp,],nrow=ni))
+    ind3PPD<-t(matrix(indPPD[keepind],nrow=ni))
+    ind3Data<-t(matrix(indData[keepind],nrow=ni))
 
     # NULL = TRUE  (true negatives, false negatives)
-    covr <- cov.mcd(ind3PPD)
-    mahN <- mahalanobis(ind3PPD, center = covr$center, cov = covr$cov, tol = 1e-25)
-    mahA <- mahalanobis(ind3Data, center = covr$center, cov = covr$cov, tol = 1e-25)
-
+    #covr <- cov.mcd(ind3PPD)
+    covr <- cov(ind3PPD)
+    #test<-svd(covr)
+    mu<-apply(ind3PPD,2,median)
+    #mahN <- mahalanobis(ind3PPD, center = covr$center, cov = covr$cov, tol = 1e-25)
+    #mahA <- mahalanobis(ind3Data, center = covr$center, cov = covr$cov, tol = 1e-25)
+    #mahN1 <- mahalanobis(ind3PPD, center = mu, cov = covr, tol = 1e-25)
+    #mahA1 <- mahalanobis(ind3Data, center = mu, cov = covr, tol = 1e-25)
+    
+    mahN <- mahalanobis_robust(ind3PPD, center = mu, cov = covr)
+    mahA <- mahalanobis_robust(ind3Data, center = mu, cov = covr)
+    mahN<-extreme.outlier(mahN)
+    mahA<-extreme.outlier(mahA)
+    
     mah[1,pp,]<-mahN
     mah[2,pp,]<-mahA
-
-    Thres<-quantile(mahN,1-alpha)
-    PRB[1,pp]<-sum(mahN>Thres)/length(mahN)   # False positive
-    PRB[2,pp]<-sum(mahA>Thres)/length(mahA)   # True positive
+  
+    Thres<-quantile(mahN,1-alpha,na.rm=T)
+    #plot(density(mah[1,pp,],na.rm=T),type="l",col="blue")
+    #lines(density(mah[2,pp,],na.rm=T),col="red")
+    #abline(v=Thres)
+    
+    
+    PRB[1,pp]<-mean(mah[1,pp,]>Thres,na.rm=T)   # False positive
+    PRB[2,pp]<-mean(mah[2,pp,]>Thres,na.rm=T)   # True positive
 
   }
-
-  list(mah=mah,PRB=PRB)
+ 
+  return(list(mah=mah,PRB=PRB,keep=keep))
 
 }
 
@@ -119,40 +140,159 @@ Probs<-function(indPPD,indData,alpha=0.05){
 #' @param MSE An object of class MSE
 #' @param hzn Time horizon for posterior data
 #' @param alpha Probability of incorrectly rejecting the null hypothesis of normal data when it is true
-#' @importFrom MVN mardiaTest roystonTest hzTest
-#' @importFrom robustbase covMcd
+#' @importFrom MASS cov.mcd
+#' @importFrom corpcor pseudoinverse
 #' @return Indicators of MSE misspecification
 #' @author T. Carruthers
 #' @references Carruthers et al. 2018
 #' @export
-EC=function(PPD,Data,res=6,styr=NA,LB=0.9,UB=0.95,plot=T){
+PRBcalc=function(MSE_null,MSE_alt,
+                 tsd= c("Cat","Cat","Cat","Ind","ML"),
+                 stat=c("slp","AAV","mu","slp", "slp"),
+                 dnam=c("CS","CV","CM","IS","MLS"),
+                 res=6,alpha=0.05,plotCC=F){
+  
+  styr<-MSE_null@nyears+1
+  
+  outlist<-new('list')
+   
+  for(mm in 1:MSE_null@nMPs){
+    
+    PPD<-MSE_null@Misc[[mm]]
+    Data<-MSE_alt@Misc[[mm]]
+  
+    indPPD<-getinds(PPD,styr=styr,res=res,tsd=tsd,stat=stat)
+    indData<-getinds(Data,styr=styr,res=res,tsd=tsd,stat=stat)
 
-  indPPD<-getinds(PPD,styr=styr,res=res)
-  indData<-getinds(Data,styr=styr,res=res)
+    if(plotCC)CC(indPPD,indData,pp=2,res=res)
 
-  if(plot)CC(indPPD,indData,pp=2,res=res)
-
-  out<-Probs(indPPD,indData)
-  np<-dim(indPPD)[2]
-  par(mfrow=c(np,1))
-
-  for(pp in 1:np){
-
-    xlim=quantile(out$mah[,pp,],c(0.01,0.99))
-    densN<-density(out$mah[1,pp,],adj=1,from=0)
-    densN$y<-densN$y/max(densN$y)
-    densA<-density(out$mah[2,pp,],adj=1,from=0)
-    densA$y<-densA$y/max(densA$y)
-
-    plot(densN,xlim=xlim,col='blue',main="")
-    lines(densA,xlim=xlim,col='red')
-
+    out<-Probs(indPPD,indData,alpha=alpha)
+    
+    outlist[[mm]]<-out
+    
+    message(paste(mm, "of",MSE_null@nMPs,"MPs processed"))
+    
   }
+  
+  return(outlist)
 
+}
 
+mahalanobis_robust<-function (x, center, covr, inverted = FALSE) {
+
+  x <- if (is.vector(x)) 
+    matrix(x, ncol = length(x))
+  else as.matrix(x)
+  if (!identical(center, FALSE)) 
+    x <- sweep(x, 2L, center)
+  
+  invcov <- pseudoinverse(covr)
+  setNames(rowSums(x %*% invcov * x), rownames(x))
+  
 }
 
 
 
+mahplot<-function(outlist,MPs){
+
+  nMP<-length(outlist)
+  ncol<-floor(nMP^(1/3))
+  nrow<-ceiling(nMP/ncol)
+  par(mai=c(0.2,0.2,0.01,0.01),omi=c(0.2,0.2,0.2,0.01))
+  layout(matrix(1:(nrow*ncol*2),nrow=nrow*2),heights=rep(c(2,1),nrow))
+  
+  for(mm in 1:nMP){
+    
+    mahdensplot(outlist[[mm]])
+    PRBplot(outlist[[mm]])
+    
+  }
+  
+}
+
+mahdensplot<-function(out,adj=0.9,alpha=0.05){  
+  
+  heightadj<-0.7
+  np<-dim(out$PRB)[2]
+  
+  densN<-new('list')
+  densA<-new('list')
+  ylimy=array(0,c(np,2))
+  thresh<-rep(0,np)
+  tfac<-3
+  xref<-rep(0,np)
+  
+  xmaxN<-0
+  xmaxA<-0
+ 
+  for(pp in 1:np){
+    thresh[pp]<-quantile(out$mah[1,pp,!is.na(out$mah[1,pp,])],1-alpha)
+    xref[pp]<-thresh[pp]*tfac
+    ylimy[pp,2]<-xref[pp]#quantile(out$mah[1,pp,],0.95,na.rm=T)*1.2
+    densN[[pp]]<-density(out$mah[1,pp,],adj=adj,from=0,to=xref[pp],na.rm=T)
+    densA[[pp]]<-density(out$mah[2,pp,],adj=adj,from=0,to=xref[pp],na.rm=T)
+    ymaxN<-max(densN[[pp]]$y)
+    ymaxA<-max(densA[[pp]]$y)
+    if(ymaxN>xmaxN)xmaxN=ymaxN
+    if(ymaxA>xmaxA)xmaxA=ymaxA
+  }
+  
+  for(pp in 1:np){
+    #densN[[pp]]$y<-densN[[pp]]$y/max(densN[[pp]]$y)*heightadj
+    #densA[[pp]]$y<-densA[[pp]]$y/max(densA[[pp]]$y)*heightadj
+    densN[[pp]]$y<-densN[[pp]]$y/xmaxN*heightadj
+    densA[[pp]]$y<-densA[[pp]]$y/xmaxA*heightadj
+  }
+  
+  plot(c(0.5,np+0.3),c(0,tfac),col='white')
+  lines(c(-100,100),rep(1,2),lty=2,lwd=1)
+  for(pp in 1:np){
+    
+    trs<-0.5+pp-1
+  
+    lines(densN[[pp]]$y+trs,densN[[pp]]$x/thresh[pp],col='blue')
+    lines(densA[[pp]]$y+trs,densA[[pp]]$x/thresh[pp],col='red')
+   
+    polyN<-getsegment(densN[[pp]],thresh[pp],lower=T)
+    polyA<-getsegment(densA[[pp]],thresh[pp],lower=F)
+    polygon(polyN$x+trs,polyN$y/thresh[pp],border=NA,col="#0000ff50")
+    
+    if(length(polyA$x)>10)polygon(polyA$x+trs,polyA$y/thresh[pp],border=NA,col="#ff000050")
+    
+  }  
+}
+
+
+PRBplot<-function(out){
+  
+  plot((1:5) - 0.2,1-out$PRB[1,],col="#0000ff50",ylim=c(0,1),type='l',lwd=2,xlab="",ylab="")
+  abline(h=0.8,lty=2)
+  lines((1:5) - 0.2,out$PRB[2,],col="#ff000050",lwd=2)
+  
+}
+
+
+getsegment<-function(densobj,thresh,lower=T){
+  
+  if(lower){
+    cond<-densobj$x<thresh
+  }else{
+    cond<-densobj$x>thresh
+  }
+  
+  xs<-c(0,densobj$y[cond],0)
+  ys<-densobj$x[cond]
+  ys<-c(ys[1],ys,ys[length(ys)])
+
+  list(x=xs,y=ys)
+
+}
+
+extreme.outlier<-function(x){
+  sd<-mean(abs(x-median(x)))
+  cond<-(x<(median(x)-3*sd(x)))|(x>(median(x)+3*sd(x)))
+  x[cond]<-NA
+  x
+}
 
 # ====================================================================
