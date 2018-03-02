6b12c95bfb9e5521dd594d12c24b44e90ab572a2
Quang Huynh <q.huynh@oceans.ubc.ca>
2018-03-02 14:40:41 -0800

    generate_report() is no longer necessary.

--- R/report_core.R
+++ R/report_core.R
@@ -1,34 +1,4 @@
 
-#' Generate assessment report
-#'
-#' Returns a list of assessment output and, optionally, a multitude of plots
-#' (e.g., residuals, predicted time series).
-#'
-#' @param Assessment An S4 object of assessment output generated from an MP.
-#' @param figure Indicates whether plots will be drawn.
-#' @param save_figure Indicates whether figures will be saved to directory.
-#' @param save_dir The directory to which figures will be saved. By default: \code{getwd()}
-#' @return A list of data and parameter inputs, predicted time series and derived quantities,
-#' and model estimates and standard deviations.
-#' Optionally, corresponding plots will also be drawn.
-#' @export generate_report
-#' @details List dataframes and figures here.
-#' @author Q. Huynh
-#' @examples
-#' dir <- tempdir()
-#' setwd(dir)
-#'
-#' output <- DD_TMB(1, Red_snapper, Assessment = TRUE)
-#' RS_report <- generate_report(output, figure = FALSE, save_dir = dir)
-generate_report <- function(Assessment, figure = TRUE, save_figure = TRUE,
-                            save_dir = getwd()) {
-  old.stringsAsFactors <- options()$stringsAsFactors
-  on.exit(options(stringsAsFactors = old.stringsAsFactors))
-  options(stringsAsFactors = FALSE)
-
-  f <- get(paste0('generate_report_', Assessment@MP))
-  f(Assessment, figure = figure, save_figure = save_figure, save_dir = save_dir)
-}
 
 #' Perform profile likelihood
 #'
