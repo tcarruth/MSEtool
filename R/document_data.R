90a6e35fe7c787b60ea3baec38cfe2f408fca402
Quang Huynh <q.huynh@oceans.ubc.ca>
2018-03-15 18:31:30 -0700

    Document datasets in package.

---
+++ R/document_data.R
@@ -0,0 +1,25 @@
+#' North Atlantic Swordfish dataset
+#'
+#' An S4 object containing catch and index time series for North Atlantic swordfish.
+#'
+#' @format An object of class Data.
+#' @source ASPIC Software at https://www.mhprager.com/aspic.html
+#'
+#' @examples
+#' data(swordfish)
+#'
+"swordfish"
+
+#' Simulated Snapper dataset
+#'
+#' An S4 object containing a simulated catch and index time series, based on Gulf of Mexico
+#' red snapper.
+#'
+#' @details Simulated catch and index with cyclic recruitment.
+#'
+#' @format An object of class Data.
+#'
+#' @examples
+#' data(sim_snapper)
+#'
+"sim_snapper"
