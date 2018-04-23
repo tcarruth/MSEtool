843b5f741759415adec10be0cd4e2d9173ac6cf0
Quang Huynh <q.huynh@oceans.ubc.ca>
2018-04-23 15:52:20 -0700

    Add diagnostics functions for Assess during MSE

--- R/Make_MP.R
+++ R/Make_MP.R
@@ -6,6 +6,8 @@
 #'
 #' @param .Assess A function of class \code{Assess}.
 #' @param .HCR A function of class \code{HCR}.
+#' @param diagnostic (TRUE/FALSE) Whether assessment output will be written to
+#' \code{\link[DLMtool]{DLMenv}}. Intended for use with \code{\link[DLMtool]{runMSE}}.
 #' @param ... Additional arguments to be passed to \code{.Assess} and \code{.HCR}.
 #'
 #' @return A function of class \code{MP}.
@@ -16,13 +18,17 @@
 #'
 #' \dontrun{
 #' myOM <- DLMtool::runMSE(DLMtool::testOM, MPs = c("FMSYref", "DD_40_10"))
+#'
+#' ls(DLMenv) # Model output during MSE is assigned to this environment.
+#' plot_diagnostic()
 #' }
 #'
 #' # MP that uses a surplus production model which assumes Binit_frac = 0.5.
 #' SP_MSY <- make_MP(SP, HCR_MSY, Binit_frac = 0.5)
-#' @export make_MP
 #' @importFrom pryr make_function
-make_MP <- function(.Assess, .HCR, ...) {
+#' @seealso \link{plot_diagnostic}
+#' @export
+make_MP <- function(.Assess, .HCR, diagnostic = TRUE, ...) {
   if(is.character(.Assess)) {
     .Assess <- as.symbol(.Assess)
   } else {
@@ -33,14 +39,27 @@ make_MP <- function(.Assess, .HCR, ...) {
   } else {
     .HCR <- substitute(.HCR)
   }
-  if(!inherits(eval(.Assess), "Assess")) stop(paste(.Assess, "does not belong to class 'Assess.'"))
-  if(!inherits(eval(.HCR), "HCR")) stop(paste(.HCR, "does not belong to class 'HCR.'"))
+  if(!inherits(eval(.Assess), "Assess")) {
+    stop(paste(.Assess, "does not belong to class 'Assess'. Use: avail('Assess') to find eligible objects."))
+  }
+  if(!inherits(eval(.HCR), "HCR")) {
+    stop(paste(.HCR, "does not belong to class 'HCR.' Use: avail('HCR') to find eligible objects."))
+  }
 
-  MP_body <- bquote({
-    do_Assessment <- .(.Assess)(x = x, Data = Data, ...)
-    Rec <- .(.HCR)(do_Assessment, reps = reps, ...)
-    return(Rec)
-  })
+  if(diagnostic) {
+    MP_body <- bquote({
+      opt_timing <- system.time(do_Assessment <- .(.Assess)(x = x, Data = Data, ...))[3]
+      Rec <- .(.HCR)(do_Assessment, reps = reps, ...)
+      Assess_diagnostic()
+      return(Rec)
+    })
+  } else {
+    MP_body <- bquote({
+      do_Assessment <- .(.Assess)(x = x, Data = Data, ...)
+      Rec <- .(.HCR)(do_Assessment, reps = reps, ...)
+      return(Rec)
+    })
+  }
 
   custom_MP <- make_function(args = alist(x = , Data = , reps = 1),
                              body = MP_body)
