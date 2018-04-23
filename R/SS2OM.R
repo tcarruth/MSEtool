9eaa11910f0bd3c9066b1d395848b48a90352b49
Quang Huynh <q.huynh@oceans.ubc.ca>
2018-04-23 16:11:17 -0700

    Re-write SS2OM for multi-season model (part of re-vamp of function).

--- R/SS2OM.R
+++ R/SS2OM.R
@@ -16,11 +16,11 @@
 #' @export SS2OM
 #' @importFrom r4ss SS_output
 SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="No source provided",
-                 Author="No author provided",printstats=F,verbose=T){
+                 Author="No author provided",printstats=FALSE,verbose=TRUE){
 
   message("-- Using function SS_output of package r4ss to extract data from SS file structure --")
 
-  replist <- r4ss::SS_output(dir=SSdir,covar=F,ncols=1000,printstats=printstats,verbose=verbose)
+  replist <- r4ss::SS_output(dir=SSdir,covar=FALSE,ncols=1000,printstats=printstats,verbose=verbose)
   #replistB <- SS_output(dir="F:/Base",covar=F,ncols=1000,printstats=printstats,verbose=verbose)
   #replistY <- SS_output(dir="F:/Base3",covar=F,ncols=1000,printstats=printstats,verbose=verbose)
 
@@ -33,17 +33,17 @@ SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="N
 
   # The trouble is that DLMtool is an annual model so if the SS model is seasonal we need to aggregate overseasons
   # The reason for the code immediately below (and length_timestep) is that some SS models just assume quarterly timesteps with no way of knowing this (other than maxage and M possibly)!
-  if(is.na(length_timestep)){
-
-    if((replist$endyr-replist$startyr+1)>100){
-      nseas<-1/replist$seasduration[1] # too many  years for industrial fishing so assumes a quarterly model
-    }else{
-      nseas=1
-    }
-
-  }else{
-    nseas<-1/length_timestep
-  }
+  #if(is.na(length_timestep)){
+  #
+  #  if((replist$endyr-replist$startyr+1)>100){
+  #    nseas<-1/replist$seasduration[1] # too many  years for industrial fishing so assumes a quarterly model
+  #  }else{
+  #    nseas=1
+  #  }
+  #}else{
+  #  nseas<-1/length_timestep
+  #}
+  nseas <- max(replist$timeseries$Seas) #alternative way to get the number of seasons
 
   if(is.null(Name)){
     OM@Name=SSdir
@@ -51,7 +51,7 @@ SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="N
     OM@Name=Name
   }
 
-  OM@nyears<-nyears<-(replist$endyr-replist$startyr+1)/nseas
+  OM@nyears<-nyears<-(replist$endyr-replist$startyr+1)#/nseas
   if(nseas==1){
     OM@CurrentYr<-replist$endyr
   }else{
@@ -64,7 +64,7 @@ SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="N
   # === Stock parameters =========================================================================================================
 
   growdat<-getGpars(replist)
-  OM@maxage<-maxage<-ceiling(length(growdat$Age)/nseas)
+  OM@maxage<-maxage<-ceiling(length(growdat$Age))
   aind<-rep(1:OM@maxage,each=nseas)[1:length(growdat$Age)]
 
   GP <- replist$Growth_Parameters
@@ -86,27 +86,32 @@ SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="N
   OM@Linf<-quantile(Linf,c(0.025,0.975))
   OM@t0=rep(GP$A_a_L0[1],2) # t0 is not
   OM@Msd<-OM@Ksd<-OM@Linfsd<-OM@Mgrad<-OM@Kgrad<-OM@Linfgrad<-c(0,0)
-  L50=GP$Mat1[1]
-  OM@a=GP$WtLen1[1]
-  OM@b=GP$WtLen2[1]
+  #L50 <- rep(GP$Mat1[1], 2)
+  OM@a <- rep(GP$WtLen1[1], 2)
+  OM@b <- rep(GP$WtLen2[1], 2)
 
-  M<-aggregate(growdat$M,by=list(aind),mean)$x
-  Wt<-aggregate(growdat$Wt_Mid,by=list(aind),mean)$x
-  Wt_age<-array(rep(Wt,each=nsim)*trlnorm(nsim,1,cvLinf), dim = c(nsim, maxage, nyears))
-  Mat<-aggregate(growdat$Age_Mat,by=list(aind),mean)$x
+  #M<-aggregate(growdat$M,by=list(aind),mean)$x
+  #Wt<-aggregate(growdat$Wt_Mid,by=list(aind),mean)$x
+  #Wt_age<-array(rep(Wt,each=nsim)*trlnorm(nsim,1,cvLinf), dim = c(nsim, maxage, nyears))
+  #Mat<-aggregate(growdat$Age_Mat,by=list(aind),mean)$x
+  M <- growdat$M
+  Wt <- growdat$Wt_Mid
+  Wt_age <- array(rep(Wt,each=nsim)*trlnorm(nsim,1,cvLinf), dim = c(nsim, maxage, nyears))
+  Mat <- growdat$Len_Mat/max(growdat$Len_Mat)
 
   # SO FAR: Wt_age K Linf
 
   surv<-c(1,exp(cumsum(-c(M[2:maxage]))))# currently M and survival ignore age 0 fish
-  OM@M<-rep(sum(M[2:maxage]*surv[2:maxage])/sum(surv[2:maxage]),2)
+  #OM@M<-rep(sum(M[2:maxage]*surv[2:maxage])/sum(surv[2:maxage]),2)
+  OM@M2 <- OM@M <- M
   SSB0<-as.numeric(replist$Dynamic_Bzero$SPB[replist$Dynamic_Bzero$Era=="VIRG"])
   SpR<-sum(Wt*Mat*surv)
-  OM@R0<-rep(SSB0/SpR,2)
+  OM@R0<-SSB0/SpR
 
   SSB<-replist$recruit$spawn_bio
   rec<-replist$recruit$pred_recr
   SSBpR<-SSB[1]/rec[1]
-  hs<-SRopt(nsim,SSB,rec,SSBpR,plot=F,type="BH")
+  hs<-SRopt(nsim,SSB,rec,SSBpR,plot=FALSE,type="BH")
   OM@h<-quantile(hs,c(0.025,0.975))
   OM@SRrel<-1 # This is the default
 
@@ -134,9 +139,8 @@ SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="N
 
   # Maturity --------------------------------------
 
-  Mat_age<-growdat$Age_Mat
-  if(max(Mat_age)>1)Mat_age<-as.numeric(Mat_age>1)
-
+  #Mat_age<-growdat$Age_Mat
+  Mat_age <- Mat
   Len_age<-growdat$Len_Mid
 
   # Currently using linear interpolation of mat vs len, is robust and very close to true logistic model predictions
@@ -158,17 +162,18 @@ SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="N
   F_at_age[nrow(F_at_age),]<-F_at_age[nrow(F_at_age)-1,]# ad-hoc mirroring to deal with SS missing predicitons of F in terminal age
   Ftab<-cbind(expand.grid(1:dim(F_at_age)[1],1:dim(F_at_age)[2]),as.vector(F_at_age))
 
-  if(nseas>1){
-    sumF<-aggregate(Ftab[,3],by=list(aind[Ftab[,1]],Ftab[,2]),mean,na.rm=T)
-    sumF<-aggregate(sumF[,3],by=list(sumF[,1],yind2[sumF[,2]]),sum,na.rm=T)
-  }else{
-    sumF<-Ftab
-  }
+  #if(nseas>1){
+  #  sumF<-aggregate(Ftab[,3],by=list(aind[Ftab[,1]],Ftab[,2]),mean,na.rm=T)
+  #  sumF<-aggregate(sumF[,3],by=list(sumF[,1],yind2[sumF[,2]]),sum,na.rm=T)
+  #}else{
+  #  sumF<-Ftab
+  #}
 
-  sumF<-sumF[sumF[,2]<nyears,] # generic solution: delete partial observation of final F predictions in seasonal model (last season of last year is NA)
+  sumF<-Ftab[Ftab[, 2]<=nyears,] # generic solution: delete partial observation of final F predictions in seasonal model (last season of last year is NA)
   V <- array(NA, dim = c(nsim, maxage, nyears + proyears))
-  V[,,1:(nyears-1)]<-rep(sumF[,3],each=nsim) # for some reason SS doesn't predict F in final year
-  V[,,nyears:(nyears+proyears)]<-V[,,nyears-1]
+  V[,,1:nyears] <- array(sumF[,3], dim = c(nsim, maxage, nyears))
+  #V[,,1:(nyears-1)]<-rep(sumF[,3],each=nsim) # for some reason SS doesn't predict F in final year
+  V[,,nyears:(nyears+proyears)]<-V[,,nyears]
 
   Find<-apply(V,c(1,3),max,na.rm=T) # get apical F
 
@@ -188,22 +193,27 @@ SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="N
 
   # -- Recruitment -----------------------------------------------
 
-  rind<-rep(1:1000,each=nseas)[1:length(replist$recruit$dev)]
-  recs<-aggregate(replist$recruit$dev,list(rind),mean,na.rm=T)$x
+  #rind<-rep(1:1000,each=nseas)[1:length(replist$recruit$dev)]
+  #recs<-aggregate(replist$recruit$dev,list(rind),mean,na.rm=T)$x
+  recs <- replist$recruit$dev
   recs<-recs[!is.na(recs)&recs!=0]
   nrecs<-length(recs)
   recdevs<-rep(0,nyears+maxage-1)
   recdevs[(nyears+maxage)-(nrecs:1)]<-recs# last year is mean recruitment
   recdevs<-recdevs[1:(nyears+maxage-2)]
-  #recdevs<-replist[length(replist$recruit$dev)-nyears)]
-  #recdevs[is.na(recdevs)]<-0
-  OM@AC<-rep(acf(recdevs[!is.na(recdevs)],plot=F)$acf[2,1,1],2)
+  recdevs<-replist[length(replist$recruit$dev)-nyears]
+  recdevs[is.na(recdevs)]<-0
+  #OM@AC<-rep(acf(recdevs[!is.na(recdevs)],plot=F)$acf[2,1,1],2)
+  OM@AC <- rep(acf(recs, plot = FALSE)$acf[2,1,1], 2)
 
   Perr<-array(NA,c(nsim,nyears+proyears+maxage-1))
-  Perr[,1:(maxage+nyears-2)]<-matrix(rnorm(nsim*(maxage+nyears-2),rep(recdevs,each=nsim),0.2),nrow=nsim) # generate a bunch of simulations with uncertainty
+  Perr[,1:(maxage+nyears-2)]<-matrix(rnorm(nsim*(maxage+nyears-2),rep(rec,each=nsim),0.2),nrow=nsim) # generate a bunch of simulations with uncertainty
   procsd<-apply(Perr,1,sd,na.rm=T)
-  OM@Perr<-quantile(procsd,c(0.025,0.975)) # uniform range is a point estimate from assessment MLE
+  #OM@Perr<-quantile(procsd,c(0.025,0.975)) # uniform range is a point estimate from assessment MLE
+  procsd <- replist$sigma_R_in
+  OM@Perr <- rep(procsd, 2)
   procmu <- -0.5 * (procsd)^2  # adjusted log normal mean
+  Perr <- array(NA, c(nsim, nyears+proyears+maxage-1))
   Perr[,(maxage+nyears-2)+(1:(proyears+1))]<-matrix(rnorm(nsim*(proyears+1),rep(procmu,proyears+1),rep(procsd,proyears+1)),nrow=nsim)
   AC<-mean(OM@AC)
   for (y in (nyears-1):(nyears + proyears)) Perr[, y] <- AC * Perr[, y - 1] +   Perr[, y] * (1 - AC * AC)^0.5
@@ -239,8 +249,8 @@ SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="N
 
   # Index observations -------------------------------------------------------
 
-  OM@Iobs<-rep(sd(log(replist$cpue$Obs)-log(replist$cpue$Exp)),2)
-  getbeta<-function(beta,x,y)sum((y-x^beta)^2)
+  OM@Iobs<-rep(sd(log(replist$cpue$Obs)-log(replist$cpue$Exp), na.rm = TRUE),2)
+  getbeta<-function(beta,x,y)sum((y-x^beta)^2,na.rm = TRUE)
   OM@beta<-rep(optimize(getbeta,x=replist$cpue$Obs,y=replist$cpue$Exp,interval=c(0.1,10))$minimum,2)
 
   #F_FMSY<-replist$derived_quants[grep("F_",replist$derived_quants[,1]),2]
@@ -269,7 +279,7 @@ SS2OM<-function(SSdir,nsim=48,proyears=50,length_timestep=NA,Name=NULL,Source="N
 #' @param zeroint is there a zero-zero x-y intercept?
 #' @author T. Carruthers
 #' @export LinInterp
-LinInterp<-function(x,y,xlev,ascending=F,zeroint=F){
+LinInterp<-function(x,y,xlev,ascending=FALSE,zeroint=FALSE){
 
   if(zeroint){
     x<-c(0,x)
@@ -308,7 +318,7 @@ LinInterp<-function(x,y,xlev,ascending=F,zeroint=F){
 #' @author T. Carruthers
 #' @export negcorlogspace
 #' @importFrom mvtnorm rmvnorm
-negcorlogspace<-function(xmu,ymu,xcv=0.1,nsim,cor=-0.9,ploty=F){
+negcorlogspace<-function(xmu,ymu,xcv=0.1,nsim,cor=-0.9,ploty=FALSE){
 
   varcov=matrix(c(1,cor,cor,1),nrow=2)
   out<-mvtnorm::rmvnorm(nsim,c(0,0),sigma=varcov)
@@ -380,7 +390,7 @@ movdistil<-function(movtab){
 #' @param plot should a plot of the model fit be produced?
 #' @author T. Carruthers
 #' @export getBH
-getBH<-function(pars,SSB,rec,SSBpR,mode=1,plot=F){
+getBH<-function(pars,SSB,rec,SSBpR,mode=1,plot=FALSE){
 
   h<-0.2+1/(1+exp(-pars[1]))*0.8
   R0<-exp(pars[2])
@@ -400,7 +410,8 @@ getBH<-function(pars,SSB,rec,SSBpR,mode=1,plot=F){
 
   if(mode==1){
     #return(sum(((recpred-rec)/10000)^2))
-    return(-sum(dnorm(log(recpred)-log(rec),0,0.5,log=T))-dnorm(pars[1],0,6,log=T)) # add a vague prior on h = 0.6
+    sigmaR <- sqrt(sum((log(rec/recpred))^2)/length(recpred))
+    return(-sum(dnorm(log(rec)-log(recpred),0,sigmaR,log=T)))#-dnorm(pars[1],0,6,log=T)) # add a vague prior on h = 0.6
     #return(-sum(dnorm(recpred,rec,rec*0.5,log=T)))
   }else{
     return(rec-recpred)
@@ -420,12 +431,12 @@ getBH<-function(pars,SSB,rec,SSBpR,mode=1,plot=F){
 #' @param plot should a plot of model fit be produced?
 #' @author T. Carruthers
 #' @export optBH
-optBH<-function(x,SSB,rec,SSBpR,R0temp,pars,frac=0.5,plot=F){
+optBH<-function(x,SSB,rec,SSBpR,R0temp,pars,frac=0.5,plot=FALSE){
 
-  samp<-sample(1:length(SSB),size=ceiling(length(SSB)*frac),replace=F)
+  samp<-sample(1:length(SSB),size=ceiling(length(SSB)*frac),replace=FALSE)
   opt<-optim(pars,getBH,method="L-BFGS-B",lower=c(-6.,log(R0temp/50)),upper=c(6.,log(R0temp*50)), hessian=T,
-           SSB=SSB[samp],rec=rec[samp],SSBpR=SSBpR,mode=1, plot=F)
-  if(plot)getBH(opt$par,SSB,rec,SSBpR,mode=2,plot=plot)
+           SSB=SSB[samp],rec=rec[samp],SSBpR=SSBpR,mode=1, plot=FALSE)
+  if(plot) getBH(opt$par,SSB,rec,SSBpR,mode=2,plot=plot)
   0.2+1/(1+exp(-opt$par[1]))*0.8
 
 }
@@ -441,7 +452,7 @@ optBH<-function(x,SSB,rec,SSBpR,R0temp,pars,frac=0.5,plot=F){
 #' @author T. Carruthers
 #' @import snowfall
 #' @export SRopt
-SRopt<-function(nsim,SSB,rec,SSBpR,plot=F,type="BH"){
+SRopt<-function(nsim,SSB,rec,SSBpR,plot=FALSE,type="BH"){
 
     R0temp<-rec[1] # have a guess at R0 for initializing nlm
     pars<-c(0,log(R0temp))
