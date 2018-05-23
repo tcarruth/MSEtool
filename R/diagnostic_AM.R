ff7e0331ec24d93beb2fcd0d2853c50ae1931dc9
Quang C. Huynh <q.huynh@oceans.ubc.ca>
2018-05-23 15:53:45 -0700

    Re-name to AM (assessment in MSE) functions

--- R/diagnostic_AM.R
+++ R/diagnostic_AM.R
@@ -1,34 +1,36 @@
 
-#' Do Assess models converge during MSE?
+#' diagnostic_AM - diagnostic of Assessments in MSE. Did Assess models converge during MSE?
 #'
 #' Diagnostic check for convergence of Assess models converge during MSE.
 #' Assess models write output to the DLMenv environement if their argument
 #' \code{diagnostic = TRUE}.
+#'
 #' @param MSE An object of class MSE created by \code{\link[DLMtool]{runMSE}}.
 #' @param DLMenv The name of the environment that contains the Assessment output
 #' generated during the MSE.
 #' @param gradient_threshold The value of the maximum gradient magnitude below which the
 #' model is considered to have converged.
-#' @return Traffic light (red/green) plots indicating whether model converged,
+#' @return Figure. Traffic light (red/green) plots indicating whether model converged,
 #' according to \code{convergence} code in list returned by \code{\link[stats]{nlminb}}
 #' the Hessian matrix is positive-definite, according to \code{pdHess} in
 #' list returned by \code{\link[TMB]{sdreport}}, and the maximum gradient magnitude is
-#' below \code{gradient_threshold}. Also, model run time during all simulations if the
+#' below \code{gradient_threshold}. Model run time during all simulations if the
 #' model was timed in the management procedure.
 #' @author Q. Huynh
 #' @examples
 #' \dontrun{
-#' DD_MSY <- makeMP(DD_TMB, HCR_MSY, diagnostic = TRUE) # is TRUE by default
+#' DD_MSY <- makeMP(DD_TMB, HCR_MSY, diagnostic = TRUE) # is FALSE by default
 #' show(DD_MSY)
 #' myMSE <- DLMtool::runMSE(DLMtool::testOM, MPs = "DD_MSY")
-#' plot_diagnostics(myMSE)
+#' diagnostic_AM(myMSE)
 #'
 #' ls(DLMtool::DLMenv) # Assessment output and diagnostics are located here
-#' save(myMSE, DLMenv, file = "DLMenv.RData") # Save to disk. Note: very large object!
+#' save(myMSE, DLMenv, myMSE_hist, file = "DLMenv.RData") # Save to disk for future use. File may be very large due to size of DLMenv!
 #' }
 #' @importFrom graphics layout
+#' @seealso \link{retrospective_AM}
 #' @export
-plot_diagnostics <- function(MSE, DLMenv = DLMtool::DLMenv, gradient_threshold = 0.1) {
+diagnostic_AM <- function(MSE, DLMenv = DLMtool::DLMenv, gradient_threshold = 0.1) {
   if(length(ls(DLMenv)) == 0) stop(paste0("Nothing found in ", DLMenv))
 
   old_par <- par(no.readonly = TRUE)
@@ -53,11 +55,7 @@ plot_diagnostics <- function(MSE, DLMenv = DLMtool::DLMenv, gradient_threshold =
     diagnostic <- objects[[2]]
 
     if(!is.null(diagnostic)) {
-      #if(!is.null(opt_time)) {
-      #  layout(matrix(c(1, 2, 3, 4, 4, 5), ncol = 3, byrow = TRUE))
-      #} else par(mfrow = c(1, 3))
-      #par(oma = c(0, 0, 2, 0))
-      par(mar = c(5, 4, 1, 1), oma = c(0, 0, 2, 0))
+      par(mar = c(5, 4, 1, 1), oma = c(0, 0, 8, 0))
       layout(matrix(c(1, 2, 3, 4, 4, 5), ncol = 3, byrow = TRUE))
 
       convergence_code <- lapply(diagnostic, get_code, y = "conv")
@@ -73,11 +71,6 @@ plot_diagnostics <- function(MSE, DLMenv = DLMtool::DLMenv, gradient_threshold =
       plot_time(opt_time, "line")
       plot_time(opt_time, "hist")
     }
-
-    #if(!is.null(opt_time)) {
-    #  plot_time(opt_time, "line")
-    #  plot_time(opt_time, "hist")
-    #}
     title(paste(MPs[i], "management procedure"), outer = TRUE)
   }
 
@@ -166,116 +159,6 @@ plot_max_gr <- function(max_gr, threshold = 1) {
   return(invisible())
 }
 
-
-retrospective_MSE <- function(MSE, DLMenv = DLMtool::DLMenv, sim = 1, MP,
-                              plot_type = c("SSB", "F", "SSB_SSBMSY", "F_FMSY", "SSB_SSB0", "VB")) {
-  if(length(ls(DLMenv)) == 0) stop(paste0("Nothing found in ", DLMenv))
-
-  old_par <- par(no.readonly = TRUE)
-  on.exit(par(old_par))
-
-  par(mfcol = c(2, 3), mar = c(5, 4, 1, 1), oma = c(0, 0, 2.5, 0))
-
-  env_objects <- ls(DLMenv)
-  MPs <- MSE@MPs
-  MPs_in_env <- vapply(MPs, function(x) any(grepl(x, env_objects)), logical(1))
-  MPs <- MPs[MPs_in_env]
-
-  plot_type <- match.arg(plot_type, several.ok = TRUE)
-  if(!MP %in% MSE@MPs || !MP %in% MPs) stop(paste(MP, "was not found in MSE object (MSE@MPs)."))
-  if(!MP %in% MPs) stop(paste(MP, "was not found in DLMenv."))
-  if(length(sim) > 1 || sim > MSE@nsim) stop(paste0(sim, " should be a number between 1 and ", MSE@nsim, "."))
-  MPind <- MSE@MPs == MP
-  if(sum(MPind) > 1) stop(paste("More than one match to", MP, "was found in MSE object."))
-
-  Assessment_report <- get(paste0("Assessment_report_", MP), envir = DLMenv)[[sim]]
-  isSP <- grepl("SP", Assessment_report[[1]]@Model)
-
-  color.vec <- gplots::rich.colors(length(Assessment_report))
-  for(i in 1:length(plot_type)) {
-    if(plot_type[i] == "SSB_SSBMSY") {
-      Hist <- apply(MSE@SSB_hist, c(1, 3), sum)[sim, ]/MSE@OM$SSBMSY[sim]
-      Proj <- MSE@B_BMSY[sim, MPind, ]
-      if(!isSP) {
-        Assess <- lapply(Assessment_report, slot, "SSB_SSBMSY")
-      } else {
-        Assess <- lapply(Assessment_report, slot, "VB_VBMSY")
-      }
-    }
-    if(plot_type[i] == "F_FMSY") {
-      Hist <- apply(MSE@FM_hist, c(1, 3), max)[sim, ]/MSE@OM$FMSY[sim]
-      Proj <- MSE@F_FMSY[sim, MPind, ]
-      #Assess <- lapply(Assessment_report, slot, "F_FMSY")
-      AssessU <- lapply(Assessment_report, slot, "U")
-      AssessUMSY <- lapply(Assessment_report, slot, "UMSY")
-      Assess <- Map(function(x, y) log(1-x)/log(1-y), x = AssessU, y = AssessUMSY)
-    }
-    if(plot_type[i] == "SSB") {
-      Hist <- apply(MSE@SSB_hist, c(1, 3), sum)[sim, ]
-      Proj <- MSE@SSB[sim, MPind, ]
-      if(!isSP) {
-        Assess <- lapply(Assessment_report, slot, "SSB")
-      } else {
-        Assess <- lapply(Assessment_report, slot, "VB")
-      }
-    }
-    if(plot_type[i] == "F") {
-      Hist <- apply(MSE@FM_hist, c(1, 3), max)[sim, ]
-      Proj <- MSE@FM[sim, MPind, ]
-      #Assess <- lapply(Assessment_report, slot, "F")
-      AssessU <- lapply(Assessment_report, slot, "U")
-      Assess <- lapply(AssessU, function(x) -log(1 - x))
-    }
-    if(plot_type[i] == "SSB_SSB0") {
-      Hist <- apply(MSE@SSB_hist, c(1, 3), sum)[sim, ]/MSE@OM$SSB0[sim]
-      Proj <- MSE@SSB[sim, MPind, ]/MSE@OM$SSB0[sim]
-      if(!isSP) {
-        Assess <- lapply(Assessment_report, slot, "SSB_SSB0")
-      } else {
-        Assess <- lapply(Assessment_report, slot, "VB_VB0")
-      }
-    }
-    if(plot_type[i] == "VB") {
-      Hist <- apply(MSE@VB_hist, c(1, 3), sum)[sim, ]
-      Proj <- MSE@VB[sim, MPind, ]
-      Assess <- lapply(Assessment_report, slot, "VB")
-    }
-
-    if(plot_type[i] != "MSY") {
-      xlimits <- c(1, MSE@nyears + MSE@proyears)
-      ylimits <- c(0, 1.1 * max(c(Hist, Proj, do.call(c, Assess))))
-      plot(c(Hist, Proj), xlab = "MSE year", ylab = plot_type[i], xlim = xlimits, ylim = ylimits, lwd = 2, typ = 'l')
-      for(j in length(Assess):1) {
-        if(length(Assess[[j]]) > 0) lines(1:length(Assess[[j]]), Assess[[j]], col = color.vec[j])
-      }
-      if(plot_type[i] == "SSB_SSBMSY" || plot_type[i] == "F_FMSY") abline(h = 1)
-      abline(h = 0, col = "grey")
-      abline(v = MSE@nyears, lty = 2)
-    }
-
-    if(plot_type[i] == "MSY") {
-      Hist <- c(MSE@OM$FMSY[sim], MSE@OM$MSY[sim])
-      Assess <- vapply(Assessment_report, function(x) c(-log(1 - slot(x, "UMSY")), slot(x, "MSY")), numeric(2))
-
-      xc <- c(Hist[1], Assess[1, ])
-      xc <- xc[xc < 2]
-      xlimits <- c(max(0, mean(xc) - 2*sd(xc)), mean(xc + 2*sd(xc))) # FMSY
-      yc <- c(Hist[2], Assess[2, ])
-      ylimits <- c(max(0, mean(yc) - 2*sd(yc)), mean(yc + 2*sd(yc))) # MSY
-      plot(x = Assess[1, ], y = Assess[2, ], xlim = xlimits, ylim = ylimits,
-           xlab = "FMSY estimates", ylab = "MSY estimates", col = color.vec, pch = 16,
-           cex = 2)
-      j <- ncol(Assess) - 1
-      arrows(x0 = Assess[1, 1:j], y0 = Assess[2, 1:j], x1 = Assess[1, 2:(j+1)], y1 = Assess[2, 2:(j+1)],
-             length = 0.1)
-      points(Hist[1], Hist[2], col = "grey", cex = 2, pch = 0)
-
-    }
-
-  }
-  title(paste0(MP, " management procedure \n Simulation #", sim), outer = TRUE)
-  invisible()
-}
 
 
 # Prints Assessment during MSE output to DLMtool::DLMenv
