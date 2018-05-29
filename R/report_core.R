caf33b56b85ad16132a56f6d95c9bae32cdcf8f5
Quang Huynh <q.huynh@oceans.ubc.ca>
2018-05-29 00:01:17 -0700

    Minor help file edits

--- R/report_core.R
+++ R/report_core.R
@@ -14,6 +14,17 @@
 #' @author Q. Huynh
 #' @return A data frame of negative log-likelihood values from profile and, optionally,
 #' a figure of the likelihood surface.
+#' @examples
+#' \dontrun{
+#' data(sim_snapper)
+#' output <- DD_TMB(Data = sim_snapper)
+#' pro <- profile_likelihood(output, UMSY = seq(0.05, 0.15, 0.01), MSY = seq(0.1, 3, 0.01))
+#'
+#' # Ensure your grid is of proper resolution.
+#' # This grid is too coarse.
+#' poor_profile <- profile_likelihood(output, UMSY = seq(0.05, 0.15, 0.1), MSY = seq(0.1, 3, 0.1),
+#' save_figure = FALSE)
+#' }
 #' @export profile_likelihood
 profile_likelihood <- function(Assessment, figure = TRUE, save_figure = TRUE,
                                save_dir = getwd(), ...) {
@@ -33,14 +44,16 @@ profile_likelihood <- function(Assessment, figure = TRUE, save_figure = TRUE,
 #' the retrospective analysis.
 #' @param save_figure Indicates whether figures will be saved to directory.
 #' @param save_dir The directory to which figures will be saved. By default: \code{getwd()}
-#' @export retrospective
 #' @author Q. Huynh
 #' @return Figures showing the time series of biomass and exploitation and parameter estimates
 #' with successive number of years removed. Returns invisibly an array of model output and of model estimates.
 #' @examples
-#' data(Snapper_sim)
-#' output <- DD_TMB(Snapper_sim)
-#' RS_retro <- retrospective(output, nyr = 5)
+#' \dontrun{
+#' data(sim_snapper)
+#' output <- DD_TMB(Data = sim_snapper)
+#' get_retro <- retrospective(output, nyr = 5)
+#' }
+#' @export
 retrospective <- function(Assessment, nyr = 5, figure = TRUE, save_figure = TRUE,
                           save_dir = getwd()) {
   f <- get(paste0('retrospective_', Assessment@Model))
