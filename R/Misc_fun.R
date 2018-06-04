b2275f6d0a4823bd79d92624574add6ec4847527
Quang C. Huynh <q.huynh@oceans.ubc.ca>
2018-06-04 10:21:03 -0700

    Add fn for rescaling catch

--- R/Misc_fun.R
+++ R/Misc_fun.R
@@ -58,3 +58,39 @@ expand_comp_matrix <- function(Data, comp_type = c("CAA", "CAL")) {
   return(Data)
 }
 
+# var_div - report variables which are divided by the catch rescale
+# var_mult - report variables which are multiplied by the catch rescale
+# var_trans - transformed variables which need to be rescaled
+# fun_trans - the function for rescaling the transformed variables (usually either "*" or "/")
+# fun_fixed - the transformation from the output variable to the estimated variable indicated in var_trans (e.g. log, logit, NULL)
+rescale_report <- function(var_div, var_mult, var_trans = NULL, fun_trans = NULL, fun_fixed = NULL) {
+  output <- mget(c("report", "rescale", "SD"), envir = parent.frame(), ifnotfound = list(NULL))
+  report <- output$report
+
+  if(!is.null(var_div)) report[var_div] <- lapply(report[var_div], "/", output$rescale)
+  if(!is.null(var_mult)) report[var_mult] <- lapply(report[var_mult], "*", output$rescale)
+  assign("report", report, envir = parent.frame())
+
+  if(!is.null(output$SD) && !is.character(output$SD)) {
+    SD <- output$SD
+    if(!is.null(var_trans)) {
+      for(i in 1:length(var_trans)) {
+        var_trans2 <- var_trans[i]
+        fun_trans2 <- fun_trans[i]
+        fun_fixed2 <- fun_fixed[i]
+
+        ind <- pmatch(var_trans2, names(SD$value))
+        SD$value[ind] <- do.call(match.fun(fun_trans2), list(SD$value[ind], output$rescale))
+        SD$sd[ind] <- do.call(match.fun(fun_trans2), list(SD$sd[ind], output$rescale))
+
+        if(!is.na(fun_fixed2)) {
+          fixed_name <- paste0(fun_fixed2, "_", var_trans2)
+          ind_fixed <- pmatch(fixed_name, names(SD$par.fixed))
+          SD$par.fixed[ind_fixed] <- do.call(match.fun(fun_fixed2), list(SD$value[var_trans2]))
+        }
+      }
+    }
+    assign("SD", SD, envir = parent.frame())
+  }
+  invisible()
+}
