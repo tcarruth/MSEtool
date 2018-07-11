b24dab2f1fa677cf3185f88801affc076d8b7212
Quang C. Huynh <q.huynh@oceans.ubc.ca>
2018-07-10 18:04:51 -0700

    Use Red_snapper instead of sim_snapper for DD_SS

--- R/document_data.R
+++ R/document_data.R
@@ -10,16 +10,3 @@
 #'
 "swordfish"
 
-#' Simulated Snapper dataset
-#'
-#' An S4 object containing a simulated catch and index time series, based on Gulf of Mexico
-#' red snapper.
-#'
-#' @details Simulated catch and index with cyclic recruitment.
-#'
-#' @format An object of class Data.
-#'
-#' @examples
-#' data(sim_snapper)
-#'
-"sim_snapper"
