cfb65fdcf0e9a4fd8f9e42d2f2fd6cb5ef13444c
Quang C. Huynh <q.huynh@oceans.ubc.ca>
2018-08-31 17:01:37 -0700

    Test some MPs on testOM

---
+++ tests/test-MSE.R
@@ -0,0 +1,26 @@
+
+
+# Test that default configurations are very, very robust when running MSEs.
+library(testthat)
+library(MSEtool)
+
+
+DD_MP <- make_MP(DD_TMB, HCR_MSY, diagnostic = 'min')
+DDSS_MP <- make_MP(DD_SS, HCR_MSY, diagnostic = 'min')
+SP_MP <- make_MP(SP, HCR_MSY, diagnostic = 'min')
+SPSS_MP <- make_MP(SP_SS, HCR_MSY, diagnostic = 'min')
+SCA_MP_dome <- make_MP(SCA, HCR_MSY, diagnostic = 'min', vulnerability = 'dome')
+SCA_MP <- make_MP(SCA, HCR_MSY, diagnostic = 'min')
+
+MP_vec <- c("DD_MP", "DDSS_MP", "SP_MP", "SPSS_MP", "SCA_MP_dome", "SCA_MP")
+
+testOM@nsim <- 200
+
+
+for(i in MP_vec) {
+  setup(12)
+  res <- runMSE(testOM, MPs = i, PPD = TRUE, parallel = TRUE)
+  cat(diagnostic_AM(res, figure = FALSE))
+  save(res, file = paste0("test_results/", i, ".RData"))
+  sfStop()
+}
